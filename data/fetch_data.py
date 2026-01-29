c498c3240237fd060d84b4b45a0792db3e9c6016
Axolotter <115641617+Axolotter@users.noreply.github.com>
2026-01-29 21:24:46 +0000

    Began initial_plots

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -1,16 +1,28 @@
-#=========Copied from UCIML==========#
+
 
 from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-fertility = fetch_ucirepo(id=244) 
-  
-# data (as pandas dataframes) 
-X = fertility.data.features 
-y = fertility.data.targets 
-  
-# metadata 
-print(fertility.metadata) 
-  
-# variable information 
-print(fertility.variables) 
+
+def get_data():  
+    #=========Copied from UCIML==========#
+    # fetch dataset 
+    fertility = fetch_ucirepo(id=244) 
+    
+    # data (as pandas dataframes) 
+    X = fertility.data.features 
+    y = fertility.data.targets 
+    
+    # metadata 
+    #print(fertility.metadata) 
+    
+    # variable information 
+    #print(fertility.variables) 
+    #====================================
+
+    feature_names = fertility.variables[fertility.variables["role"] == 'Feature']["name"].tolist()
+    target_name = fertility.variables[fertility.variables["role"] == "Target"]["name"].values[0]
+
+    df = pd.DataFrame(fertility.data.features, columns=feature_names)
+    df[target_name] = fertility.data.targets
+
+    return df, target_name
+
---
+++ data/initial_plots.py
@@ -0,0 +1,28 @@
+from data.fetch_data import get_data
+import matplotlib as plt
+import os
+
+def make_plot(var1, var2):
+    df, target_name = get_data()
+
+    os.makedirs("plots", exist_ok = true)
+
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(
+        data=df,
+        x=var1,
+        y=var2,
+        hue=target_name,
+        style=target_name,
+        s=90
+    )
+
+
+    plt.title(f"Fertility Diagnosis: {var1} vs {var2}")
+    plt.xlabel(var1)
+    plt.ylabel(var2)
+    plt.grid(True)
+    plt.savefig(f'data/plots/{var1}_v_{var2}.png', dpi=150)
+    plt.close()
+
+make_plot()
