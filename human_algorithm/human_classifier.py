0e20a65805c2d26090b0eba4748fbc8d683740af
Axolotter <115641617+Axolotter@users.noreply.github.com>
2026-02-19 21:30:03 +0000

    Human model training complete, need work on graphs

--- human_algorithm/human_classifier.py
+++ human_algorithm/human_classifier.py
@@ -1,11 +1,9 @@
-def human_sort(df):
-    acc = df['accident']
-    surg = df['surgical_intervention']
-    smoke = df['smoking']
-    if(acc.eq(0)):
-        if(smoke.eq(1) & surg.eq(0)):
+def human_sort(acc, surg, smoke):
+    
+    if(acc == 0):
+        if(smoke == 1 & surg == 0):
             return "O"
-        elif(surg.eq(1)) & smoke.ne(1):
+        elif(surg == 1) & smoke != 1:
             return "O"
         else:
             return "N"
--- human_algorithm/train_human_model.py
+++ human_algorithm/train_human_model.py
@@ -12,75 +12,81 @@ df, target_name = get_data()
 train_df, test_df = train_test_split(
     df,
     test_size=.9,
-    random_state=42,
+    random_state=44,
     stratify=df[target_name]
 )
 
-def test_sort(row, dF):
-    var1 = dF.empty
-    return var1
+# def test_sort(surg, acc):
+#     if acc==1:
+#         return "O"
+#     else:
+#         return "N"
     
-# print(test_df[['accident', 'surgical_intervention', 'smoking']])
-# print(test_sort(test_df))
 
-test_df['human_prediction'] = test_df['accident'].apply(test_sort, args=df["accident"])
-print(test_df['human_prediction'])
-# # test_df['human_prediction'] = test_df['accident'].apply(human_sort, args=test_df[['accident', 'surgical_intervention', 'smoking']])
-# test_df['correct'] = test_df['human_prediction'] == test_df[target_name]
-# accuracy = (test_df['human_prediction'] == test_df[target_name]).mean()
-# print(f"Human classifier accuracy: {accuracy:.2%}")
+test_df['human_prediction'] = None
+for index, row in test_df.iterrows():
+    surg = row['surgical_intervention']
+    acc = row['accident']
+    smoke = row['smoking']
+    test_df.loc[index, 'human_prediction'] = human_sort(acc, surg, smoke)
+
+# print(test_df['human_prediction'])
+
+test_df['correct'] = test_df['human_prediction'] == test_df[target_name]
+accuracy = (test_df['human_prediction'] == test_df[target_name]).mean()
+print(f"Human classifier accuracy: {accuracy:.2%}")
 
 
-# # Here we print the confusion matrix to see how well the human classifier performed on the test-data subset.
-# conf_matrix = pd.crosstab(
-#     test_df[target_name],
-#     test_df['human_prediction'],
-#     rownames=['Actual'],
-#     colnames=['Predicted']
+# Here we print the confusion matrix to see how well the human classifier performed on the test-data subset.
+conf_matrix = pd.crosstab(
+    test_df[target_name],
+    test_df['human_prediction'],
+    rownames=['Actual'],
+    colnames=['Predicted']
+)
+print(conf_matrix)
+
+# # Finally, we print one example of a failure case where the human classifier got the prediction wrong.
+# failure_row = test_df[test_df['human_prediction'] != test_df[target_name]].iloc[0]
+# print("\nFAILURE EXAMPLE")
+# print(failure_row[['sepal width', 'petal width', target_name, 'human_prediction']])
+
+
+# Print a scatter plot showing correct vs incorrect predictions.
+os.makedirs("human_algorithm/plots", exist_ok=True)
+
+target_colors = list(map(lambda s: 'g' if s=='correct' else 'w', test_df[target_name]))
+# print(target_colors)
+
+sns.set_style("whitegrid", {'axes.grid' : False})
+
+fig = plt.figure(figsize=(6,6))
+
+ax = fig.add_subplot(111, projection='3d')
+
+x = 'accident'
+y = 'surgical_intervention'
+z = 'smoking'
+ax.scatter(x, y, z, c=target_colors, data=test_df, marker='o')
+# cmap='RdYlGn_r'
+#'accident', 'surgical_intervention', 'smoking'
+ax.set_xlabel("accident")
+ax.set_ylabel("surgical_intervention")
+ax.set_zlabel("smoking")
+# plt.legend(title='Prediction Correct')
+plt.title(f"Fertility Diagnosis: accident vs surgical_intervention vs smoking")
+
+plt.savefig(f'human_algorithm/plots/human_model_training_results.png.png', dpi=150)
+
+# plt.figure(figsize=(8, 6))
+# sns.scatterplot(
+#     data=test_df,
+#     x='sepal width',
+#     y='petal width',
+#     hue='correct',
+#     style='correct',
+#     s=100,
+#     palette={True: 'green', False: 'red'}
 # )
-# print(conf_matrix)
 
-# # # Finally, we print one example of a failure case where the human classifier got the prediction wrong.
-# # failure_row = test_df[test_df['human_prediction'] != test_df[target_name]].iloc[0]
-# # print("\nFAILURE EXAMPLE")
-# # print(failure_row[['sepal width', 'petal width', target_name, 'human_prediction']])
-
-
-# # Print a scatter plot showing correct vs incorrect predictions.
-# os.makedirs("human_algorithm/plots", exist_ok=True)
-
-# target_colors = list(map(lambda s: 'g' if s=='correct' else 'r', test_df[target_name]))
-# # print(target_colors)
-
-# sns.set_style("whitegrid", {'axes.grid' : False})
-
-# fig = plt.figure(figsize=(6,6))
-
-# ax = fig.add_subplot(111, projection='3d')
-
-# x = 'accident'
-# y = 'surgical_intervention'
-# z = 'smoking'
-# ax.scatter(x, y, z, c=target_colors, data=test_df, marker='o')
-# # cmap='RdYlGn_r'
-# #'accident', 'surgical_intervention', 'smoking'
-# ax.set_xlabel("accident")
-# ax.set_ylabel("surgical_intervention")
-# ax.set_zlabel("smoking")
-# # plt.legend(title='Prediction Correct')
-# plt.title(f"Fertility Diagnosis: accident vs surgical_intervention vs smoking")
-
-# plt.savefig(f'human_algorithm/plots/human_model_training_results.png.png', dpi=150)
-
-# # plt.figure(figsize=(8, 6))
-# # sns.scatterplot(
-# #     data=test_df,
-# #     x='sepal width',
-# #     y='petal width',
-# #     hue='correct',
-# #     style='correct',
-# #     s=100,
-# #     palette={True: 'green', False: 'red'}
-# # )
-
-# plt.close()
+plt.close()
